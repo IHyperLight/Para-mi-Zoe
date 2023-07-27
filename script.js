200e2a7b2ffebdd0068e79c6beae7e83dc45af91
203721 <blacky.0412@hotmail.com>
2023-07-27 14:17:34 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const lines = document.querySelectorAll('.line, .title, .subtitle');
+    const reloadButton = document.querySelector('.reload-button');
+    
+    const showLines = () => {
+        lines.forEach(line => {
+            const delay = line.getAttribute('data-delay') * 2000;
+            setTimeout(() => {
+                line.style.opacity = "1";
+                line.style.transform = "translateY(0)";
+            }, delay);
+        });
+        setTimeout(() => {
+            reloadButton.style.opacity = "1";
+        }, lines.length * 2000);
+    };
+    
+    showLines();
+    
+    reloadButton.addEventListener('click', () => {
+        lines.forEach(line => {
+            line.style.opacity = "0";
+            line.style.transform = "translateY(1rem)";
+        });
+        reloadButton.style.opacity = "0";
+        showLines();
+    });
+});
